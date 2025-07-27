60c172b2d6fb392611c3ff08c64327c09b748d12
Jimin <tonnossi@gmail.com>
2025-07-27 15:29:51 +0900

    Update Episodic.py
    #2 #8 remaining_n : 데이터 셋이 바뀌면 초기화하는 기능 구현

--- code/trainer/Episodic.py
+++ code/trainer/Episodic.py
@@ -26,8 +26,14 @@ class EpisodicTrainer(NonEpisodicTrainer):
         # EpisodicTrainer 고유 
         self.max_iter_same_interval = max_iter_same_interval
         self.remaining_n = self.max_iter_same_interval
+        self.prev_dataset_flag = -1
 
     def switch_state(self, env, state):
+        if self.prev_dataset_flag != self.dataset_flag:
+             # 데이터 셋이 바뀌면 remaining_n을 업데이트한다. 
+             self.prev_dataset_flag = self.dataset_flag
+             self.remaining_n = self.max_iter_same_interval
+
         if self.remaining_n > 0 and env.info in ['bankrupt']:
                 self.remaining_n -= 1
                 print(f">>>>> reset the env : {env.info} occured. Go Back To Start.")
