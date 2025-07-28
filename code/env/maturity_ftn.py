bf2d1dd11480c069e63a19fbbca33ce1ab76bebf
sabina381 <sabina2378@ewhain.net>
2025-07-28 20:48:54 +0900

    만기일 함수 오류 수정

--- code/env/maturity_ftn.py
+++ code/env/maturity_ftn.py
@@ -4,27 +4,42 @@ def calculate_maturity(dates):
     입력 받은 모든 dates 중에서 만기일을 계산해 list로 반환하는 함수
     만기일: 매월 두 번째 목요일
     '''
-    month = 0   # 월 마다 한 번이므로 월을 추적하기 위한 것
+    month = 0   # 월 추적 변수
     maturity_list = []
 
     for date in dates:
-        if date.month != month:
-            if date.weekday() <= 3: # 새 월이 시작되면 계산 시작 (1주차: 첫 번째 목요일이 있는 주)
+        # 시작 날짜가 해당 월의 만기일 이후인 경우 스킵
+        if date.day > 14:
+            continue
+        
+        # 월이 바뀌면 실행
+        if date.month == month:
+            continue
+
+        else:
+            if (date.weekday() <= 3) or ((date.weekday() == 4) & (date.day > 1)): # 새 월이 시작되면 계산 시작 (1주차: 첫 번째 목요일이 있는 주)
                 year = date.year
                 month = date.month
-                yearweek = date.isocalendar().week  # 주차
-                check_week = dates[(dates.year == year) & (dates.isocalendar().week == yearweek + 1)] # 월의 2주차 날짜 모두 가져오기
+                yearweek = date.isocalendar().week  # 기준 주차
                 
-                # 1주차를 확인해야하는 경우의 예외 처리
-                if len(check_week) <= 1:
-                    # 조건: 2주차에 장이 열리지 않음 / 금요일만 장이 열림
-                    if (len(check_week) == 0) or check_week[0].isocalender().week == 4: # 0인 경우에 뒷 조건 확인 하면 에러날텐데 or이라 상관 없는지?
-                        check_week = dates[(dates.year == year) & (dates.isocalendar().week == yearweek)]
+                # 1주차에 장이 열리지 않은 경우의 예외 처리
+                if date.day > 7:
+                    check_week = dates[(dates.year == year) & (dates.isocalendar().week == yearweek)]   # 월의 2주차
+                
+                # 1주차에 장이 열린 경우
+                else:
+                    check_week = dates[(dates.year == year) & (dates.isocalendar().week == yearweek + 1)] # 월의 2주차
+                
+                    # 1주차를 확인해야하는 경우의 예외 처리
+                    if len(check_week) <= 1:
+                        # 조건: 2주차에 장이 열리지 않음 / 금요일만 장이 열림
+                        if (len(check_week) == 0) or (check_week[0].isocalender().week == 4):
+                            check_week = dates[(dates.year == year) & (dates.isocalendar().week == yearweek)]   # 월의 1주차
 
-                for date in reversed(check_week): # 해당 주차의 날짜를 거꾸로 확인
-                    if date.weekday() <= 3: # 목요일이 만기일 / 목요일이 없는 경우 목요일 이전 가장 가까운 날짜가 만기일
-                        maturity = date.date()  # datetime.date(yyyy, mm, dd)
-                        maturity_list.append(maturity)
+                for d in reversed(check_week): # 해당 주차의 날짜를 거꾸로 확인
+                    if d.weekday() <= 3: # 목요일이 만기일 / 목요일이 없는 경우 목요일 이전 가장 가까운 날짜가 만기일
+                        maturity = d.date()  # datetime.date(yyyy, mm, dd)
+                        maturity_list.append((date, maturity))
                         break
     
     return maturity_list
