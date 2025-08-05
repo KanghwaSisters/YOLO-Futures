9f6133358d32bc8ee11b400f53fea6017be02fd8
sabina381 <sabina2378@ewhain.net>
2025-08-05 16:38:05 +0900

    update DLinear codes
    - DLinear/Decompositon: zero padding -> replicate padding
    - test_DLinear: DLinear 학습 메인 코드
    - dlinear_models 폴더: 학습시킨 모델 저장

--- code/models/DLinear.py
+++ code/models/DLinear.py
@@ -2,21 +2,28 @@ import torch
 import torch.nn as nn
 
 class Decomposition(nn.Module):
-    def __init__(self, window_size, channel_size):
+    def __init__(self, window_size, channel_size, device):
         super().__init__()
         self.window_size = window_size
-        # self.padding = (self.window_size - 1) / 2
+        self.channel_size = channel_size
+        self.device = device
+        self.padding = self.window_size // 2
 
         self.layer = nn.Conv1d(in_channels = channel_size, 
                                out_channels = channel_size, 
                                kernel_size = window_size, 
                                bias = False, 
-                               padding = 'same',
-                               dtype = torch.float64)
+                               padding = self.padding,
+                               padding_mode='replicate',
+                               groups = channel_size,
+                               dtype = torch.float32,
+                               device = self.device)
         
         # 이동 평균이므로 가중치가 모두 동일해야 함
+        weight = torch.ones(channel_size, 1, self.window_size, device = self.device) / self.window_size
+        self.register_buffer('weight', weight)
+
         with torch.no_grad():
-            self.weight = torch.ones(channel_size, 1, self.window_size) / self.window_size
             self.layer.weight.copy_(self.weight)
         self.layer.weight.requires_grad = False
 
@@ -26,19 +33,21 @@ class Decomposition(nn.Module):
         return trend, remainder
 
 class DLinearModel(nn.Module):
-    def __init__(self, window_size, seq_len, pred_len, channel_size):
+    def __init__(self, window_size, seq_len, pred_len, channel_size, device):
         super().__init__()
+        self.window_size = window_size
         self.seq_len = seq_len      # history L timesteps
         self.pred_len = pred_len    # future T timesteps
+        self.channel_size = channel_size
+        self.device = device
 
-        self.decomposition = Decomposition(window_size, channel_size)
+        self.decomposition = Decomposition(self.window_size, self.channel_size, self.device)
 
-        self.trend_layer = nn.Linear(self.seq_len, self.pred_len, dtype=torch.float64)
-        self.remainder_layer = nn.Linear(self.seq_len, self.pred_len, dtype=torch.float64)
+        self.trend_layer = nn.Linear(self.seq_len, self.pred_len, dtype=torch.float32, device=self.device)
+        self.remainder_layer = nn.Linear(self.seq_len, self.pred_len, dtype=torch.float32, device=self.device)
 
 
     def forward(self, x):
-        x = x.transpose(1,2)    # (batch_size, channel_size, seq_len)
         trend, remainder = self.decomposition(x)            # X_t, X_s
         trend_pred = self.trend_layer(trend)                # H_t
         remainder_pred = self.remainder_layer(remainder)    # H_s
